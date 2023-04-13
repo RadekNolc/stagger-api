ceeef34b1d32a00b762099b15fe07a960fdf6eab
Radek Nolč <radek.nolc@icloud.com>
2023-04-14 00:50:50 +0200

    Initial set-up

---
+++ src/main/java/cz/radeknolc/stagger/StaggerApplication.java
@@ -0,0 +1,13 @@
+package cz.radeknolc.stagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/radeknolc/stagger/StaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.radeknolc.stagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
