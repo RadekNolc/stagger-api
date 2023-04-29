9869c0369d703f8f7a9a2a7b917df3b29f51a38c
Radek Nolč <radek.nolc@icloud.com>
2023-04-29 23:24:58 +0200

    Authentication test

---
+++ src/test/java/cz/radeknolc/stagger/AuthenticationTest.java
@@ -0,0 +1,93 @@
+package cz.radeknolc.stagger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.radeknolc.stagger.model.request.LoginRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AuthenticationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    @Sql("/schema.sql")
+    @Sql("/data.sql")
+    public void beforeEach() {
+
+    }
+
+    @Test
+    public void testAuthenticationSuccess() throws Exception {
+        // Administrator authentication
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("admin", "admin"))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("admin"))
+                .andExpect(jsonPath("$.language").isNotEmpty())
+                .andExpect(jsonPath("$.token").isNotEmpty())
+                .andExpect(jsonPath("$.roles").isArray());
+
+        // User authentication
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("user", "user"))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("user"))
+                .andExpect(jsonPath("$.language").isNotEmpty())
+                .andExpect(jsonPath("$.token").isNotEmpty())
+                .andExpect(jsonPath("$.roles").isArray());
+    }
+
+    @Test
+    public void testAuthenticationErrors() throws Exception {
+        // Not existing username & password
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("test", "123"))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.content").value("BAD_CREDENTIALS"));
+
+        // Blank fields
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new LoginRequest("", ""))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.content").value("BAD_CREDENTIALS"));
+
+        // Wrong password
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("admin", "wrongpassword"))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.content").value("BAD_CREDENTIALS"));
+
+        // Disabled account
+        mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new LoginRequest("inactive", "inactive"))))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.content").value("ACCOUNT_DISABLED"));
+    }
+}
